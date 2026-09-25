b375024ed01ddbbb362e735976b6fdb7a50cce3e
Tiago Vilas Boas <tcarvalhovb@gmail.com>
2026-09-25 06:21:34 -0300

    fix(scrub): mask CPF with any separator position

--- examples/vue-nuxt-init.ts
+++ examples/vue-nuxt-init.ts
@@ -66,7 +66,7 @@ const DEDUP_WINDOW_MS = 10_000;
 const recentErrors = new Map<string, number>();
 
 const CARD_PATTERN = /\b(?:\d[ -]*?){13,19}\b/g;
-const DOCUMENT_PATTERN = /\b\d{3}[.\s-]\d{3}[.\s-]?\d{3}[.\s-]?\d{2}\b/g;
+const DOCUMENT_PATTERN = /(?<!\d)(?!\d{11})\d{3}[.\s-]?\d{3}[.\s-]?\d{3}[.\s-]?\d{2}(?!\d)/g;
 const RAW_CPF_PATTERN = /(?<!\d)\d{11}(?!\d)/g;
 const PHONE_PATTERN = /\+\d[\d\s().-]{8,16}\d/g;
 const EMAIL_PATTERN = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/gi;
